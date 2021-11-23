ab3d4bbd3760efc5bef90f8f7cd9ac77f9a9b501
devicepc <69884110+devicepc@users.noreply.github.com>
2021-11-22 22:58:53 -0300

    Add files via upload

---
+++ js/serch.js
@@ -0,0 +1,48 @@
+function muestra_oculta(id){
+    if (document.getElementById){ //se obtiene el id
+    var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
+    el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+    }
+    }
+    window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
+    muestra_oculta('cont1Faq');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
+    }
+
+function muestra_oculta2(id){
+        if (document.getElementById){ //se obtiene el id
+        var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+        }
+        }
+        window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
+        muestra_oculta2('cont2Faq');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
+        }
+function muestra_oculta3(id){
+            if (document.getElementById){ //se obtiene el id
+            var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
+            el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+            }
+            }
+            window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
+            muestra_oculta3('cont3Faq');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
+            }
+
+function muestra_oculta4(id){
+                if (document.getElementById){ //se obtiene el id
+                var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
+                el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+                }
+                }
+                window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
+                muestra_oculta4('cont4Faq');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
+                }
+
+ function muestra_oculta5(id){
+                    if (document.getElementById){ //se obtiene el id
+                    var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
+                    el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+                    }
+                    }
+                    window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
+                    muestra_oculta4('cont5Faq');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
+                    }
